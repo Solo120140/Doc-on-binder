26eb3bd85b396f7c54f1584c6b807220043bd7e2
Solo120140 <153219427+Solo120140@users.noreply.github.com>
2024-11-03 18:26:45 +0530

    Update miner.js

--- miner.js
+++ miner.js
@@ -3,8 +3,54 @@ const { chromium } = require('playwright');
 (async () => {
   // Launch a headless browser
   const browser = await chromium.launch({
-    headless: true  // Set to false if you need to see the browser for debugging
-  });
+    headless: true,
+       args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--ignore-certificate-errors',
+        '--ignore-certificate-errors-spki-list',
+        "--disable-gpu",
+        "--disable-infobars",
+        "--window-position=0,0",
+        "--ignore-certifcate-errors",
+        "--ignore-certifcate-errors-spki-list",
+        "--disable-speech-api", // 	Disables the Web Speech API (both speech recognition and synthesis)
+        "--disable-background-networking", // Disable several subsystems which run network requests in the background. This is for use 									  // when doing network performance testing to avoid noise in the measurements. ↪
+        "--disable-background-timer-throttling", // Disable task throttling of timer tasks from background pages. ↪
+        "--disable-backgrounding-occluded-windows",
+        "--disable-breakpad",
+        "--disable-client-side-phishing-detection",
+        "--disable-component-update",
+        "--disable-default-apps",
+        "--disable-dev-shm-usage",
+        "--disable-domain-reliability",
+        "--disable-extensions",
+        "--disable-features=AudioServiceOutOfProcess",
+        "--disable-hang-monitor",
+        "--disable-ipc-flooding-protection",
+        "--disable-notifications",
+        "--disable-offer-store-unmasked-wallet-cards",
+        "--disable-popup-blocking",
+        "--disable-print-preview",
+        "--disable-prompt-on-repost",
+        "--disable-renderer-backgrounding",
+        "--disable-setuid-sandbox",
+        "--disable-sync",
+        "--hide-scrollbars",
+        "--ignore-gpu-blacklist",
+        "--metrics-recording-only",
+        "--mute-audio",
+        "--no-default-browser-check",
+        "--no-first-run",
+        "--no-pings",
+        "--no-sandbox",
+        "--no-zygote",
+        "--password-store=basic",
+        "--use-gl=swiftshader",
+        "--use-mock-keychain"
+      ],
+      ignoreHTTPSErrors: true,
+    });
 
   // Open a new browser page
   const page = await browser.newPage();
